76a5cad2cfe656c186efa38bdf0a78febcb28a29
likelong <247907lkl>
2022-11-11 00:44:51 +0800

    202.快乐数（链表是否成环问题）

--- src/main/java/com/lkl/leetcode/hashtable/Sub202.java
+++ src/main/java/com/lkl/leetcode/hashtable/Sub202.java
@@ -2,31 +2,70 @@ package com.lkl.leetcode.hashtable;
 
 /**
  * 快乐数
- *
+ * `然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1`
  * @author likelong
  * @date 2022/11/10
  */
 public class Sub202 {
     public boolean isHappy(int n) {
-        if (n == 1 || n == 7) {
+//        if (n == 1 || n == 7) {
+//            return true;
+//        }
+//
+//        String num = String.valueOf(n);
+//        int sum = 0;
+//        for (int i = 0; i < num.length(); i++) {
+//            sum += Math.pow(Integer.parseInt(num.charAt(i) + ""), 2);
+//        }
+//
+//        num = sum + "";
+//        while (String.valueOf(sum).length() != 1) {
+//            sum = 0;
+//            for (int i = 0; i < num.length(); i++) {
+//                sum += Math.pow(Integer.parseInt(num.charAt(i) + ""), 2);
+//            }
+//            num = sum + "";
+//        }
+//        return sum == 1 || sum == 7;
+
+        if(n == 1){
             return true;
         }
 
-        String num = String.valueOf(n);
-        int sum = 0;
-        for (int i = 0; i < num.length(); i++) {
-            sum += Math.pow(Integer.parseInt(num.charAt(i) + ""), 2);
+        //为什么会无限循环？ 找快乐数的过程找到了相同的数，相当于链表成环了，
+        //因此，题目就演变成了判断链表是否成环
+        /*
+        以2为例： 2->4->16->37->58->89->145->42->20
+                    |__________________________/
+        所以链表只要成环了就会无限循环下去，此数就不是快乐数
+        否则只要最后链表末尾元素值为1就是快乐数
+         */
+        //第一种：使用哈希表记录元素，再次出现相同元素说明链表成环 （哈希法）
+//        List<Integer> cache = new ArrayList<>();
+//        while(n != 1){
+//            cache.add(n);
+//            n = getNextNum(n);
+//            if (cache.contains(n)){
+//                return false;
+//            }
+//        }
+//        return true;
+
+        //第二种：双指针，fast、slow，fast指针一次移动两个节点，slow指针一次移动一个节点
+        //fast指针要是能与slow相遇，说明链表成环（fast指针以一个节点的移动速度在环内追赶slow指针，最后一定会相遇）
+        //否则链表不能成环
+        int fast = n ,slow = 0;
+        while(fast != slow){
+            n = getNextNum(n);
+            //指针向后移动一位
+            slow = n;
+            fast = getNextNum(fast);
+            //指针向后移动两位
+            fast = getNextNum(fast);
         }
 
-        num = sum + "";
-        while (String.valueOf(sum).length() != 1) {
-            sum = 0;
-            for (int i = 0; i < num.length(); i++) {
-                sum += Math.pow(Integer.parseInt(num.charAt(i) + ""), 2);
-            }
-            num = sum + "";
-        }
-        return sum == 1 || sum == 7;
+        //快乐数fast = slow = 1，非快乐数 fast = slow != 1
+        return fast == 1;
     }
 
     public int getNextNum(int n){
@@ -40,9 +79,7 @@ public class Sub202 {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Sub202().isHappy(1111111));
+        System.out.println(new Sub202().isHappy(19));
         System.out.println(new Sub202().isHappy(2));
-
-        System.out.println(new Sub202().getNextNum(13));
     }
 }
--- src/main/java/com/lkl/leetcode/hashtable/Sub350.java
+++ src/main/java/com/lkl/leetcode/hashtable/Sub350.java
@@ -19,36 +19,36 @@ public class Sub350 {
         List<Integer> ans = new ArrayList<>();
         //方式二：借助Map
         // key是数字，value是数字出现次数
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        for (int num : nums2) {
-            Integer count = map.get(num);
-
-            if (count != null && count > 0){
-                ans.add(num);
-                //--count 先减再赋值，此时数已经-1 ; count-- 先赋值再-1，此时数字还未发生改变
-                map.put(num, --count);
-            }
-        }
-
-        //方式三：将两数组排序，双指针法，不需要借助额外空间
-//        Arrays.sort(nums1);
-//        Arrays.sort(nums2);
-//        //双指针
-//        int i = 0,j = 0;
-//        while (i < nums1.length && j < nums2.length){
-//            if (nums1[i] < nums2[j]) {
-//                i++;
-//            }else if (nums1[i] > nums2[j]){
-//                j++;
-//            }else {
-//                ans.add(nums1[i]);
-//                i++;
-//                j++;
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int num : nums1) {
+//            map.put(num, map.getOrDefault(num, 0) + 1);
+//        }
+//        for (int num : nums2) {
+//            Integer count = map.get(num);
+//
+//            if (count != null && count > 0){
+//                ans.add(num);
+//                //--count 先减再赋值，此时数已经-1 ; count-- 先赋值再-1，此时数字还未发生改变
+//                map.put(num, --count);
 //            }
 //        }
+
+        //方式三：将两数组排序，双指针法，不需要借助额外空间
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        //双指针
+        int i = 0,j = 0;
+        while (i < nums1.length && j < nums2.length){
+            if (nums1[i] < nums2[j]) {
+                i++;
+            }else if (nums1[i] > nums2[j]){
+                j++;
+            }else {
+                ans.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
         return ans.stream().mapToInt(x -> x).toArray();
     }
 
