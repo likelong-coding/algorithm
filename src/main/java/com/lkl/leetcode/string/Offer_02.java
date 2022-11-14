7e71d989c2b86af8f207d8a03d1e7b827722a72f
likelong <247907lkl>
2022-11-15 00:40:02 +0800

    替换空格——双指针法，原数组扩充，从后往前遍历（注意结束循环的条件）

--- src/main/java/com/lkl/leetcode/string/Offer_02.java
+++ src/main/java/com/lkl/leetcode/string/Offer_02.java
@@ -27,15 +27,15 @@ public class Offer_02 {
         //将原数组扩容
         char[] newCh = Arrays.copyOf(ch, ch.length + count * 2);
         //双指针从后往前遍历，两个指针分别指向新旧数组末尾！！！ 后面的指针追上了前面的指针说明前面已经没有空格了，直接结束循环
-        // i -> new ; j -> old
-        for (int i = newCh.length - 1, j = ch.length - 1; j < i; i--, j--) {
-            if (ch[j] != ' ') {
-                newCh[i] = ch[j];
+        int j = newCh.length - 1;
+        for (int i = ch.length - 1; i < j; i--) {
+            if (ch[i] != ' ') {
+                newCh[j--] = ch[i];
             } else {
-                newCh[i] = '0';
-                newCh[i - 1] = '2';
-                newCh[i - 2] = '%';
-                i -= 2;
+                newCh[j] = '0';
+                newCh[j - 1] = '2';
+                newCh[j - 2] = '%';
+                j -= 3;
             }
         }
         return new String(newCh);
