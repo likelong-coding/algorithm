8c78bf97775a605687bfc742bfc482bb4b813cf5
likelong <247907lkl>
2022-11-26 19:55:06 +0800

    459.重复的子字符串

--- src/main/java/com/lkl/leetcode/string/Sub459.java
+++ src/main/java/com/lkl/leetcode/string/Sub459.java
@@ -8,38 +8,46 @@ import org.junit.jupiter.api.Test;
  */
 public class Sub459 {
     public boolean repeatedSubstringPattern(String s) {
+        //拼接去头去尾包含匹配
+//        String str = s + s;
+//        return str.substring(1, str.length() - 1).contains(s);
 
-        if (s.length() == 1) {
+        int[] next = getNext(s);
+        if (next[next.length - 1] == 0) {
             return false;
         }
 
-        StringBuilder sub = new StringBuilder();
-        //获得子串
-        for (int i = 0; i < s.length(); i++) {
-            if (i > 0 && sub.charAt(0) == s.charAt(i)) {
-                break;
+        //看原字符串长度是否能除尽子字符串长度
+        return s.length() % (s.length() - next[next.length - 1]) == 0;
+    }
+
+
+    /**
+     * 如果是重复子字符串，前缀表最后一个元素就是最长的公共前后缀长度
+     * 而子字符串就是除去最长公共前后缀的部分
+     */
+    public int[] getNext(String s) {
+        int[] next = new int[s.length()];
+        next[0] = 0;
+
+        int j = 0;
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(i) != s.charAt(j)) {
+                j = next[j - 1];
             }
-            sub.append(s.charAt(i));
-        }
 
-        if (sub.length() > 0 && s.length() % sub.length() != 0) {
-            return false;
-        }
-
-
-        for (int i = sub.length(); i < s.length(); ) {
-            for (int j = 0; j < sub.length(); ) {
-                if (s.charAt(i++) != sub.charAt(j++)) {
-                    return false;
-                }
+            if (s.charAt(i) == s.charAt(j)) {
+                j++;
             }
+
+            next[i] = j;
         }
 
-        return true;
+        return next;
     }
 
     @Test
     public void test() {
-        System.out.println(repeatedSubstringPattern("abaaba"));
+        System.out.println(repeatedSubstringPattern("acac"));
     }
 }
