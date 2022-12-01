6cad1817dea22844fce3f4f4fbefda6086bca4c1
likelong <likelong>
2022-12-01 18:09:49 +0800

    71. 简化路径（待完成）

--- src/main/java/com/lkl/leetcode/stackAndQueue/Sub71.java
+++ src/main/java/com/lkl/leetcode/stackAndQueue/Sub71.java
@@ -1,8 +1,40 @@
 package com.lkl.leetcode.stackAndQueue;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author likelong
  * @date 2022/12/1
  */
 public class Sub71 {
+
+    public String simplifyPath(String path) {
+
+        StringBuilder sb = new StringBuilder();
+
+        char[] ch = path.toCharArray();
+        for (int i = 0; i < ch.length; ) {
+            char c = ch[i];
+            if (c != '.') {
+                sb.append(c);
+            } else {
+//                if ()
+
+                if (sb.length() - 1 > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+            i++;
+        }
+
+        if (ch.length - 1 > 1 && sb.charAt(sb.length() - 1) == '/') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return sb.toString();
+    }
+
+    @Test
+    public void test() {
+        System.out.println(simplifyPath("/../"));
+    }
 }
