fa3ca4a97051dd3058baa907fe7240ab05af7857
likelong <likelong123>
2022-12-30 11:09:12 +0800

    组合问题

---
+++ src/main/java/com/lkl/leetcode/backtracking/LC216.java
@@ -0,0 +1,75 @@
+package com.lkl.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author likelong
+ * @date 2022/12/30
+ */
+public class LC216 {
+
+    /**
+     * 最后返回结果
+     */
+    List<List<Integer>> ans = new ArrayList<>();
+    /**
+     * 单一结果 同时用于回溯
+     */
+    List<Integer> list = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        if (k > n) {
+            return ans;
+        }
+
+        backtracking(k, n, 0, 1);
+        return ans;
+    }
+
+    /**
+     * 回溯
+     *
+     * @param k         k个数组合
+     * @param targetSum 目标和
+     * @param sum       当前和
+     * @param start     开始位置（下标）
+     */
+    public void backtracking(int k, int targetSum, int sum, int start) {
+        // 当前和大于目标和直接return;  剪枝
+        if (sum > targetSum) {
+            return;
+        }
+
+        // 列表中元素数量 大于组合数量 return; 剪枝
+        if (list.size() > k) {
+            return;
+        }
+
+        // 递归终止条件
+        if (list.size() == k && targetSum == sum) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        // 剪枝 树的宽度入手
+        for (int i = start; i <= 10 - (k - list.size()); i++) {
+            list.add(i);
+            sum += i;
+            // 递归
+            System.out.println("递归之前 => " + list);
+            backtracking(k, targetSum, sum, i + 1);
+            // 递归之后回溯，撤销处理结果
+            list.remove(list.size() - 1);
+            sum -= i;
+            System.out.println("递归之前 => " + list);
+        }
+    }
+
+    public static void main(String[] args) {
+        LC216 lc216 = new LC216();
+
+        List<List<Integer>> ans = lc216.combinationSum3(3, 7);
+        System.out.println(ans);
+    }
+
+}
---
+++ src/main/java/com/lkl/leetcode/backtracking/LC77.java
@@ -0,0 +1,49 @@
+package com.lkl.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author likelong
+ * @date 2022/12/29
+ */
+public class LC77 {
+
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        backtracking(1, n, k);
+        return ans;
+    }
+
+    public void backtracking(int start, int end, int k) {
+        if (path.size() == k) {
+            // 元素数量达到目标数量，存放结果
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        // 剪枝，去除没必要的循环 搜索起点的上界 + 接下来要选择的元素个数 - 1 = n(end)
+        // 搜索上界 = n + 1 - 接下来要选择的元素个数
+        // n + 1 - (k - path.size())  for循环 树的宽度 剪枝 从树的宽度入手
+        for (int i = start; i <= end + 1 - (k - path.size()); i++) {
+            path.add(i);
+            System.out.println("递归之前 => " + path);
+            // 递归 start 开始位置，不停向后移动 递归之后回溯撤销处理结果 树的深度
+            backtracking(i + 1, end, k);
+            // 回溯，撤销处理结果
+            path.remove(path.size() - 1);
+            System.out.println("递归之后 => " + path);
+        }
+    }
+
+    public static void main(String[] args) {
+        LC77 solution = new LC77();
+        int n = 4;
+        int k = 4;
+        List<List<Integer>> res = solution.combine(n, k);
+        System.out.println(res);
+    }
+
+}
