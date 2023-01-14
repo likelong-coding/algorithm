0c8a6f313309e94421a98a57d8ab39fab43a1515
likelong <247907lkl>
2023-01-14 10:52:23 +0800

    区间问题

--- src/main/java/com/lkl/leetcode/greedy/MergeInterval.java
+++ src/main/java/com/lkl/leetcode/greedy/MergeInterval.java
@@ -11,14 +11,9 @@ import java.util.List;
  */
 public class MergeInterval {
     public int[][] merge(int[][] intervals) {
-        // 按开始位置从小到大排序，开始位置相同时，结束位置从大到小排序
-        // 保证上一个元素区间范围较大 尽量把大的区间放在前面方便操作
-        Arrays.sort(intervals, (o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o2[1] - o1[1];
-            }
-            return o1[0] - o2[0];
-        });
+        // 按开始位置从小到大排序，只需处理结束位置即可
+        // 处理一边将可能较大的区间排在前面，开始位置从小到大排序，结束位置则是从大到小排序 【大区间在前】
+        Arrays.sort(intervals, (o1, o2) ->  o1[0] - o2[0]);
 
         List<int[]> ans = new LinkedList<>();
         // 先将第一个数对放入结果 可能直接就是最大区间范围
